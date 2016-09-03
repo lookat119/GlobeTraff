d70d149c0a0dbddfa01daada21972731697048be
Melon Li <lookat119@163.com>
2016-09-03 18:28:08 +0800

    copy project from http://mm.aueb.gr/research/GlobeTraff.zip;

---
+++ locality/freqsize.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+'''
+Function: Processes a 2-column proxy trace format and prints
+          print out format: docid frequency, and size info
+author:   melonli
+date:     2016.9.3
+'''
+
+import sys
+import os
+import socket
+import argparse
+
